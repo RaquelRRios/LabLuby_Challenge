d4522088e5c254cb28e4492c116513811bf05a02
RaquelRRios <raquelribeirorios93@gmail.com>
2021-02-17 22:55:34 -0300

    1

--- rac_log.js
+++ rac_log.js
@@ -73,7 +73,7 @@ function compareArrays(arr1, arr2) {
 
 compareArrays([1, 2, 3, 4], [1, 2, 3, 4]);
 
-/*8)Implemente um método que remova os aninhamentos de um array de arrays para um array unico.
+/*8) Implemente um método que remova os aninhamentos de um array de arrays para um array unico.
 	Entrada do método ([1, 2, [3], [4, 5]]), Resultado do método: [1, 2, 3, 4, 5]*/
 
 function splitArray(arr) {
